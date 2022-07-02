61ea59acf0a61322185076fb51d868cc9f7bd95e
shane-droid <61736830+shane-droid@users.noreply.github.com>
2022-07-02 11:55:31 +1000

    Add project files.

---
+++ EdgeTxAudioApp/Models/SpeachFileModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdgeTxAudioApp.Models
+{
+    public class SpeachFileModel
+    {
+        public string Id { get; set; }
+        public string SourceText { get; set; }
+        public string Translation { get; set; }
+        public string Context { get; set; }
+        public string Path { get; set; }
+        public string Filename { get; set; }
+
+        //public SpeachFileModel(string id, string sourceText, string translation, string context, string path, string filename)
+        //{
+        //    Id = id;
+        //    SourceText = sourceText;
+        //    Translation = translation;
+        //    Context = context;
+        //    Path = path;
+        //    Filename = filename;
+        //}
+    }
+}
